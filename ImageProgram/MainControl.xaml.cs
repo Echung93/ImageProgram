134db863ddacbf3e370f7f0eb4fd4eaa0746b186
충훈 이 <kty309@gmail.com>
2021-12-02 18:01:30 +0900

    초기 커밋.

---
+++ ImageProgram/MainControl.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace ImageProgram
+{
+    /// <summary>
+    /// MainControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    /// 
+
+    public partial class MainControl : UserControl
+    {
+        public MainControl()
+        {
+            InitializeComponent();
+            statusText.Text = "패스워드를 입력해주세요.";
+            IDInput.Text = "아이디를 입력해주세요.";
+            this.IDInput.PreviewTextInput += Input_TextInput;
+            this.IDInput.TextChanged += IDInput_TextChanged;
+
+
+        }
+        public string id { get; set; }
+        public string password { get; set; }
+
+        public void btn_recent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_image_Click(object sender, RoutedEventArgs e)
+        {
+
+            Upload upload2 = new Upload()
+            {
+                id = IDInput.Text,
+                password = PWInput.Password
+            };
+
+        }
+
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (PWInput.Password != "")
+            {
+                PWInput.Password += e.Handled = !IsNumeric(e.Text);
+            }
+
+            else
+            {
+                statusText.Text = "패스워드를 입력해주세요.";
+            }
+        }
+
+        private void Input_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void IDInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private bool IsNumeric(string source)
+        {
+            Regex regex = new Regex("[a-zA-Z_0-9]");
+
+            return !regex.IsMatch(source);
+        }
+    }
+}
---
+++ ImageProgram/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageProgram
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainControl mainControl = new MainControl();
+        UserControl1 userControl1 = new UserControl1();
+        TestPage testPage = new TestPage();
+        Window testWin;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainGrid.Children.Add(mainControl);
+
+            mainControl.btn_image.Click += new RoutedEventHandler(btn_image_Click);
+            mainControl.btn_recent.Click += btn_recent_Click;
+            mainControl.btn_test.Click += btn_test_Click;
+            userControl1.btn_back.Click += btn_back_Click;
+        }
+
+        public void btn_back_Click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Clear();
+            MainGrid.Children.Add(mainControl);
+        }
+
+        public void btn_recent_Click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Clear();
+            MainGrid.Children.Add(userControl1);
+        }
+
+        public void btn_image_Click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Clear();
+            MainGrid.Children.Add(userControl1);
+            userControl1.wp.Children.Clear();
+
+            // 이미지 추가
+            ImageItem image = new ImageItem();
+            ImageItem image1 = new ImageItem();
+            userControl1.wp.Children.Add(image);
+            userControl1.wp.Children.Add(image1);
+        }
+
+
+        public void btn_test_Click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Clear();
+            MainGrid.Children.Add(testPage);
+
+
+            //testWin = new TestWin();
+            //testWin.Owner = this;
+            //testWin.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+
+            //this.Hide();
+            //testWin.ShowDialog();
+            //this.Show();
+        }
+    }
+}
